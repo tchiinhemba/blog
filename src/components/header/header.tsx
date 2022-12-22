5aecee1fe182399f546f9aff10a0bd3ffe1b414c
tchiinhemba <tchiinhemba@gmail.com>
2022-12-22 13:17:51 +0100

    update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,57 @@
-
-import {FiGithub, BsMedium, FaLinkedinIn} from 'react-icons/all'
+import {
+  FiGithub,
+  BsMedium,
+  FaLinkedinIn,
+  AiOutlineSearch,
+  CiCompass1,
+} from "react-icons/all";
+import "./main.scss";
 
 export function Header() {
-    return (
-        <header>
-            <div>
-                <span className="logo"></span>
-                <div>
-                    <ul>
-                        <li>Categories</li>
-                        <li>About Me</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <div><input type="text" /></div>
-                <div>
-                    <ul>
-                        <li><FiGithub /></li>
-                        <li><BsMedium/></li>
-                        <li><FaLinkedinIn/></li>
-                    </ul>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="header_container">
+      <div className="container">
+        <div className="menu_side">
+          <div className="logo">
+            <span className="logo_icon">
+              <CiCompass1 />
+            </span>
+            <span className="logo_text">Atlas</span>
+          </div>
+          <div className="nav_container">
+            <ul className="element_list">
+              <li>Categories</li>
+              <li>About Me</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+        <div className="social_side">
+          <div className="search_container">
+            <span className="">
+              <AiOutlineSearch />
+            </span>
+            <input
+              className="search_input"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+          <div className="social_icons-container">
+            <ul className="icons_list">
+              <li>
+                <FiGithub />
+              </li>
+              <li>
+                <BsMedium />
+              </li>
+              <li>
+                <FaLinkedinIn />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
