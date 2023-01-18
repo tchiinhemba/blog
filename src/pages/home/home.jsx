77e0d73a59fec1350f2ffe4463fefc625d6b97cd
tchiinhemba <tchiinhemba@gmail.com>
2023-01-18 17:09:59 +0100

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,68 @@
+import "./main.scss";
 
-
-
-
+import {
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillInstagram,
+  BsPinterest,
+  AiFillYoutube,
+  RiCompassFill,
+} from "react-icons/all";
 
 export function Home() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <header className="header__wrapper">
+        <div className="header__content-one">
+          <div className="logo__container">
+            <span className="icon">
+              <RiCompassFill />
+            </span>
+            <span className="logo">Atlas</span>
+          </div>
+          <div className="nav__container">
+            <ul>
+              <li>Categories</li>
+              <li>About Me</li>
+              <li>Contact</li>
+            </ul>
+          </div>
         </div>
-    ) 
+        <div className="header__content-two">
+          <div className="search__input-container">
+            <input type="text" placeholder="search..." />
+          </div>
+          <div className="social__icons">
+            <ul>
+              <li>
+                <a href="#">
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <AiFillFacebook />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <AiFillInstagram />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsPinterest />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <AiFillYoutube />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
